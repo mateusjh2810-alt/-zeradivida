2590134548d812afda5ebc43ce761ec388bffdd7
mateusjh2810-alt <mateusjh2810@gmail.com>
2026-06-11 17:21:01 -0300

    Update todo page

---
+++ app/todo/page.js
@@ -0,0 +1,10 @@
+import TodoApp from '@/components/TodoApp';
+
+export const metadata = {
+  title: 'Meu To-Do List | Serasa',
+  description: 'Aplicação de Lista de Tarefas com Local Storage',
+};
+
+export default function TodoPage() {
+  return <TodoApp />;
+}
